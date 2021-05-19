4d1060f5b8af5bf3708b30d4f6a9240b6037372e
William <william.helmrath@live.com>
2021-05-19 13:40:10 -0400

    destructure props directly in function definition

--- src/Hourly.js
+++ src/Hourly.js
@@ -1,15 +1,15 @@
-import React, { } from "react";
+import React from "react";
 import DateConverter from "./DateConverter"
 import GetIcon from "./GetIcon"
 
-function Hourly(forecast, displayToggle) {
+function Hourly({forecast, displayToggle}) {
     if (forecast.forecast !== null && displayToggle) {
         return (
             <div style={{ display: "flex", height: "1000px", flexDirection: "column", alignContent: "center" }}>
 
                 <div style={{ display: "flex", height: "100px", flexDirection: "row", alignItems: "center", flexWrap: "wrap", padding: "20px", marginLeft: "10px" }}>
                     {
-                        forecast.forecast.map((hour) => (
+                        forecast.map((hour) => (
                             <div style={{ display: "flex", height: "250px", width: "150px", flexDirection: "column", alignItems: "center", border: "dotted", borderColor: "darkslategray", flexWrap: "wrap", backgroundColor: "darkgray", padding: "10px", margin: "4px", borderRadius: "10px"}}>
                                 <h5>{DateConverter(hour.dt)[0] + ", " + DateConverter(hour.dt)[1] + " " + DateConverter(hour.dt)[2]}</h5>
                                 <h6 style={{ marginTop: "-8px", padding: "-10px" }}>{DateConverter(hour.dt)[3]}</h6>
--- src/Weekly.js
+++ src/Weekly.js
@@ -2,14 +2,14 @@ import React, { } from "react";
 import DateConverter from "./DateConverter"
 import GetIcon from "./GetIcon"
 
-function Weekly(forecast, displayToggle) {
+function Weekly({forecast, displayToggle}) {
     if (forecast.forecast !== null && displayToggle) {
         return (
             <div style={{ display: "flex", height: "1000px", flexDirection: "column", alignContent: "center" }}>
 
                 <div style={{ display: "flex", height: "100px", flexDirection: "row", alignItems: "center", flexWrap: "wrap", padding: "20px", marginLeft: "10px"}}>
                     {
-                        forecast.forecast.map((day) => (
+                        forecast.map((day) => (
                             <div style={{ display: "flex", height: "250px", width: "150px", flexDirection: "column", alignItems: "center", border: "dotted", borderColor: "darkslategray", flexWrap: "wrap", backgroundColor: "darkgray", padding: "10px", margin: "4px", borderRadius: "10px"}}>
                                 <h5>{DateConverter(day.dt)[0] + ", " + DateConverter(day.dt)[1] + " " + DateConverter(day.dt)[2]}</h5>
                                 {GetIcon(day.weather[0].icon)}
